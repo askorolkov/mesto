3f59bcf3f0154669affc58355446d8894f0362bc
Aleksey Korolkov <askorolkov1@gmail.com>
2022-11-28 19:41:25 +0500

    post review fix

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,31 @@
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close'); 
+const closeButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
-const saveButton = document.querySelector('.popup__save');
 let formElement = document.querySelector('.popup__container');
-const like = document.querySelectorAll('.photo__like');
+let nameInputField = document.getElementById('formName');
+let jobInputField = document.getElementById('formJob');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
 
 function openPopup() {
   popup.classList.add('popup_open');
+  //обнуляем введенные в поля значения при каждом новом
+  //открытии попапа
+  nameInputField.value = '';
+  jobInputField.value ='';
 }
 
-function closePopup(event) {
+function closePopup() {
   popup.classList.remove('popup_open');
 }
 
-//слушатель событий на кнопки лайк под каждой фотографией
-for (let i=0;i<like.length;i++) {
-  like[i].addEventListener('click', function(){like[i].classList.toggle('photo__like_active')});
-} 
-
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
-let nameInput = formElement.querySelectorAll('.popup__input')[0];
-let jobInput = formElement.querySelectorAll('.popup__input')[1];
-
-console.log(document.querySelector('.profile__name'));
-
 function handleFormSubmit (evt) {
-  evt.preventDefault();     
-  document.querySelector('.profile__name').textContent = nameInput.value;
-  document.querySelector('.profile__description').textContent = jobInput.value;
+  evt.preventDefault();
+  profileName.textContent = nameInputField.value;
+  profileJob.textContent = jobInputField.value;
   closePopup();
 }
 
-formElement.addEventListener('submit', handleFormSubmit); 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
