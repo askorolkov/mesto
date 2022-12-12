4dbd4b38a8faeecbbcda36d333bf565c8922e0bc
Aleksey Korolkov <askorolkov1@gmail.com>
2022-12-12 22:42:34 +0500

    post review fix

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 //кнопки
 const profileEditButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelectorAll('.popup__close');
+const buttonsClosePopup = document.querySelectorAll('.popup__close');
 const photoCloseButton = document.querySelector('.photo__popup-close');
-const addPlaceButton = document.querySelector('.profile__add-button');
+const buttonAddPlace = document.querySelector('.profile__add-button');
 //попапы
 const profileEditPopup = document.querySelector('#editProfile');
 const placeAddPopup = document.querySelector('#addPlace');
@@ -10,124 +10,86 @@ const photoPopup = document.querySelector('.photo__popup');
 //шаблоны
 const placeTemplate = document.querySelector('#place').content;
 //поля ввода
-let placeInputLink = document.querySelector('#placeLink');
-let placeInputName = document.querySelector('#placeName');
-let nameInputField = document.querySelector('#formName');
-let jobInputField = document.querySelector('#formJob');
+const placeInputLink = document.querySelector('#placeLink');
+const placeInputName = document.querySelector('#placeName');
+const nameInputField = document.querySelector('#formName');
+const jobInputField = document.querySelector('#formJob');
 //формы
 const profileForm = document.querySelector('#profileForm');
 const placeForm = document.querySelector('#placeForm');
 //остальные элементы
-const photo = document.querySelector('.photo');
-const photoElements = document.querySelectorAll('.photo__element');
+const photoList = document.querySelector('.photo');
 const photoFullscreen = document.querySelector('.photo__fullscreen-image');
 const photoText = document.querySelector('.photo__text');
-let formElement = document.querySelectorAll('.popup__container');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__description');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__description');
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+//Функция для формирования карточек из шаблона
+function createCard(photoLink,locationName) {
+  const placeCard = placeTemplate.querySelector('.photo__wrapper').cloneNode(true);
+  const photoElement = placeCard.querySelector('.photo__element');
+  photoElement.src = photoLink;
+  photoElement.alt = locationName;
+  placeCard.querySelector('.photo__location').textContent = locationName;
+  photoElement.addEventListener('click', ()=>openPopup(photoPopup));
+  placeCard.querySelector('.photo__like').addEventListener('click', (evt)=>evt.target.classList.toggle('photo__like_active'));
+  placeCard.querySelector('.photo__delete').addEventListener('click', ()=>placeCard.remove());
+  return placeCard
+}
 
-//Функция для добавления карточек из шаблона
-function addNewPlace(link,name) {
-  const placeElement = placeTemplate.querySelector('.photo__wrapper').cloneNode(true);
-  placeElement.querySelector('.photo__element').src = link;
-  placeElement.querySelector('.photo__element').alt = name;
-  placeElement.querySelector('.photo__location').textContent = name;
-  placeElement.querySelector('.photo__element').addEventListener('click', openPhotoFullscreen);
-  placeElement.querySelector('.photo__like').addEventListener('click', (evt)=>evt.target.classList.toggle('photo__like_active'));
-  placeElement.querySelector('.photo__delete').addEventListener('click', ()=>placeElement.remove());
-  photo.prepend(placeElement);
+//функция для добавления карточек на страницу
+function renderCard(photoLink, locationName) {
+  const card = createCard(photoLink, locationName);
+  photoList.prepend(card);
 }
 
 //Добавляем 6 дефолтных карточек из шаблона циклом
-for (let j=0;j<initialCards.length;j++) {
-  let link = initialCards[j]['link'];
-  let placeName = initialCards[j]['name'];
-  addNewPlace(link, placeName);
+initialCards.forEach((card)=> renderCard(card['link'],card['name']));
+
+//функция открытия попапов
+function openPopup(popup) {
+  popup.classList.add('popup_open');
 }
-
-// Функции открытия попапов
-function openEditPopup() {
-  profileEditPopup.classList.add('popup_open');
-  // Подставляем данные в форму из профиля
-  nameInputField.value = profileName.textContent;
-  jobInputField.value = profileJob.textContent;
-}
-
-function openPlacePopup() {
-  placeAddPopup.classList.add('popup_open');
-}
-
-function openPhotoFullscreen(evt) {
-  photoFullscreen.src = evt.srcElement.currentSrc;
-  photoText.textContent = evt.srcElement.alt;
-  photoPopup.classList.add('popup_open');
-}
-
-
-//Цикл для добавления слушателей событий на кнопки закрытия форм
-closeButton.forEach((button)=> button.addEventListener('click', closePopup));
 
 //функция закрытия попапов
-function closePopup(evt) {
-  let pop = evt.target.closest('.popup');
-  let photoPop = evt.target.closest('.photo__popup');
-  if (pop) {
-    evt.target.closest('.popup').classList.remove('popup_open');
-  }
-  if (photoPop) {
-    evt.target.closest('.photo__popup').classList.remove('popup_open');
-  }
+function closePopup(popup) {
+  popup.classList.remove('popup_open');
 }
 
-//Функция отправки формы
-function handleFormSubmit (evt) {
+//Цикл для добавления слушателей событий на кнопки закрытия форм
+buttonsClosePopup.forEach((button)=>{
+  const buttonParentPopup = button.closest('.popup');
+  button.addEventListener('click', ()=>closePopup(buttonParentPopup));
+});
+
+//Функции отправки формы
+function handleProfileFormSubmit (evt,popup) {
   evt.preventDefault();
-  let edit = evt.target.closest('#editProfile')
-  let add = evt.target.closest('#addPlace')
-  //если это форма редактирования профиля - подставляем значения в поля ввода
-  if (edit) {
-    profileName.textContent = nameInputField.value;
-    profileJob.textContent = jobInputField.value;
-  }
-  //если это форма добавления места - вызываем функцию добавления карточки
-  //и обнуляем поля ввода
-  if (add) {
-    addNewPlace(placeInputLink.value, placeInputName.value);
-    placeInputLink.value = '';
-    placeInputName.value = '';
-  }
-  closePopup(evt);
-}
+  profileName.textContent = nameInputField.value;
+  profileJob.textContent = jobInputField.value;
+  closePopup(popup);
+};
 
-profileEditButton.addEventListener('click', openEditPopup);
-addPlaceButton.addEventListener('click', openPlacePopup);
-photoCloseButton.addEventListener('click',closePopup);
-//циклом добавляем обработчики событий на обе формы
-formElement.forEach((item)=> item.addEventListener('submit', handleFormSubmit));
+function handlePlaceFormSubmit (evt,popup) {
+  evt.preventDefault();
+  renderCard(placeInputLink.value, placeInputName.value);
+  closePopup(popup);
+  placeForm.reset();
+};
+
+profileEditButton.addEventListener('click',()=>openPopup(profileEditPopup));
+
+//добавляем второй слушатель клика на попап редактирования профиля
+//для подстановки данных профиля в поля ввода
+profileEditButton.addEventListener('click',()=> {
+  nameInputField.value = profileName.textContent;
+  jobInputField.value = profileJob.textContent;
+});
+
+
+buttonAddPlace.addEventListener('click', ()=>openPopup(placeAddPopup));
+photoCloseButton.addEventListener('click',()=>closePopup(photoPopup));
+
+//добавляем обработчики событий на сабмиты форм редактирования профиля и добавления карточек
+profileForm.addEventListener('submit',(evt)=> handleProfileFormSubmit(evt,profileEditPopup));
+placeForm.addEventListener('submit',(evt)=> handlePlaceFormSubmit(evt,placeAddPopup));
